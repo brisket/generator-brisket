10f5ce3031b774afd057c1a4b1ca461af4d5d525
wawjr3d <wawjr3d@gmail.com>
2016-06-14 01:26:59 -0400

    remove unnecessary base modeling

--- app/templates/app/javascripts/base/BaseCollection.js
+++
@@ -1,7 +0,0 @@
-import Brisket from 'brisket';
-
-const BaseCollection = Brisket.Collection.extend({
-  // add properties here that you want to expose to all of your Collections
-});
-
-export default BaseCollection;
